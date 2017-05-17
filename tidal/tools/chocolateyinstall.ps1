65d174b3aee3577ed48a6dac12f63a9595d8c664
Mike Hodnick <mike@kindohm.com>
2017-05-17 05:47:19 -0700

    perform Atom TidalCycles package install

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -1,8 +1,15 @@
 ﻿$packageName= 'tidalcycles'
 
+# refresh env vars after other packages have been installed
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 # todo
-# 1. install superdirt quark
-# 2. stack setup
-# 3. stack install tidal
-# 4. Atom tidalcycles package apm install
-# 5. default dirt samples ?
+# - Atom tidalcycles package apm install
+# - install superdirt quark
+# - stack setup
+# - stack install tidal
+# - default dirt samples ?
+
+# install tidalcycles Atom package
+Write-Host 'Installing TidalCycles Atom package.'
+apm install tidalcycles
