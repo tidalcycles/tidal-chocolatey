0c700ef9b42431a00e7da43880012443198b4790
alex <alex@slab.org>
2020-09-03 23:15:17 +0100

    guess at a vscode version

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -3,15 +3,15 @@
 # refresh env vars after other packages have been installed
 
 $scpath = Join-Path $env:programfiles "SuperCollider-3.9.3"
-$apmpath = Join-Path $env:localappdata "atom\bin"
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scpath + ";" + $apmpath
+$codepath = Join-Path $env:programfiles "Microsoft VS Code"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scpath + ";" + $codepath
 
-### install tidalcycles Atom package
-Write-Host 'Installing TidalCycles Atom package (using apm in $apmpath).'
-apm install tidalcycles
+### install tidalcycles Code package
+Write-Host 'Installing the TidalCycles VS Code extension'
+code --install-extension tidalcycles.vscode-tidalcycles
 
-get-childitem $env:localappdata
-get-childitem $apmpath
+#get-childitem $env:localappdata
+#get-childitem $apmpath
 
 ### install SuperDirt
 $quarkinstall_path = $env:ChocolateyPackageFolder + '\tools\quarkinstall.sc'
@@ -19,7 +19,7 @@ Write-Host 'Installing SuperDirt sound synth and sample library. This will proba
 sclang $quarkinstall_path
 
 # Finally, install Tidal
-Write-Host "Installing tidal library. This will also take a long time." 
+Write-Host "Installing Tidal library. This will also take a long time." 
 cabal v1-update
 cabal v1-install tidal
 
