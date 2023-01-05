21656dea90c571711b347b580561810291df9f75
Alex McLean <alex@slab.org>
2023-01-05 13:36:55 +0000

    Update chocolateyinstall.ps1

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $apmpath = Join-Path $env:localappdata "atom\bin"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scpath + ";" + $apmpath
 
 ### install tidalcycles Atom package
-Write-Host 'Installing TidalCycles Atom package (using apm in $apmpath).'
-apm install tidalcycles
+#Write-Host 'Installing TidalCycles Atom package (using apm in $apmpath).'
+#apm install tidalcycles
 
 get-childitem $env:localappdata
 get-childitem $apmpath
@@ -25,3 +25,6 @@ cabal v1-install tidal
 
 Write-Host 'Done.'
 Write-Host 'd1 $ sound "bd sn"'
+
+Write-Host 'Please note that this installer has not installed an editor. For a complete Tidal system, we recommend you install pulsar.'
+Write-Host 'Please see: http://tidalcycles.org/docs/getting-started/editor/Pulsar'
