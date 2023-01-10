b549735d50d6c5f05fba3a6f70010e527a41bf7f
HighHarmonics <highharmonics@gmail.com>
2023-01-10 00:00:27 -0800

    update chocolateyinstall.ps1 with Pulsar install
    steps added to install pulsar without having a package in Chocolatey

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 # refresh env vars after other packages have been installed
 
 $scpath = Join-Path $env:programfiles "SuperCollider-3.12.1"
-$apmpath = Join-Path $env:localappdata "atom\bin"
+$apmpath = Join-Path $env:localappdata "Programs\Pulsar\app\ppm\bin"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scpath + ";" + $apmpath
 
 ### install tidalcycles Atom package
@@ -17,13 +17,29 @@ $quarkinstall_path = $env:ChocolateyPackageFolder + '\tools\quarkinstall.sc'
 Write-Host 'Installing SuperDirt sound synth and sample library. This will probably take a long time.'
 sclang $quarkinstall_path
 
-# Finally, install Tidal
+### install Tidal
 Write-Host "Installing tidal library. This will also take a long time." 
 cabal update
 cabal v1-install tidal
 
-Write-Host 'Done.'
+### install pulsar via download
+# NOTE: can be removed once pulsar is available in chocolatey
+$wc = New-Object System.Net.WebClient
+$pulsarUrl = "https://download.pulsar-edit.dev/?os=windows&type=windows_setup"
+$downloadPath = "env:HOMEPATH\Downloads\pulsarInstall.exe"
+Write-Host "Downloading Pulsar to $downloadPath"
+$wc.DownloadFile($pulsarUrl, $downloadPath)
+
+Write-Host "Installing Pulsar - this may take awhile"
+Start-Process -FilePath $downloadPath
+cd $apmpath
+Write-Host "Installing Tidalcycles package to Pulsar"
+.\apm.cmd install tidalcycles
+Write-Host "Pulsar Install completed"
+
+Write-Host 'Done with Install.'
 Write-Host 'd1 $ sound "bd sn"'
 
-Write-Host 'Please note that this installer has not installed an editor. For a complete Tidal system, we recommend you install pulsar.'
-Write-Host 'Please see: http://tidalcycles.org/docs/getting-started/editor/Pulsar'
+# Write-Host 'Please note that this installer has not installed an editor. For a complete Tidal system, we recommend you install pulsar.'
+Write-Host "If there were problems with the Pulsar install, you may need to manually install."
+Write-Host "Please see: http://tidalcycles.org/docs/getting-started/editor/Pulsar"
