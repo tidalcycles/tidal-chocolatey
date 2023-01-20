f9f785a77014f00889eb0dd08ccc3e9d90a70f77
HighHarmonics <highharmonics@gmail.com>
2023-01-19 17:21:34 -0800

    update powershell install commands
    removed commented Atom install commands
    changed SuperDirt install command to cd to $scpath first (covers scenario where sclang won't run)
    Changed $quarkinstall_path to not use $env:ChocolateyPackageFolder - in case that env value isn't set 
    expanded $env:path settings
      add SuperCollider and Pulsar paths to $env:Path
    added comments back to user with more details
      
      reload current path from registry

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -1,24 +1,22 @@
 ﻿$packageName= 'tidalcycles'
 
-# refresh env vars after other packages have been installed
+# write new paths for SuperCollider, Pulsar and Pulsar apm 
+#   then load machine and user path values 
 
-$scpath = Join-Path $env:programfiles "SuperCollider-3.12.1"
-$apmpath = Join-Path $env:localappdata "Programs\Pulsar\app\ppm\bin"
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scpath + ";" + $apmpath
-
-### install tidalcycles Atom package
-#Write-Host 'Installing TidalCycles Atom package (using apm in $apmpath).'
-#apm install tidalcycles
-#get-childitem $env:localappdata
-#get-childitem $apmpath
+$scPath = Join-Path $env:programfiles "SuperCollider-3.12.1"
+$apmPath = Join-Path $env:localappdata "Programs\Pulsar\app\ppm\bin"
+$pulsarPath = "$env:localappdata\Programs\Pulsar"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scPath + ";" + $apmPath + ";" + $pulsarPath
 
 ### install SuperDirt
-$quarkinstall_path = $env:ChocolateyPackageFolder + '\tools\quarkinstall.sc'
-Write-Host 'Installing SuperDirt sound synth and sample library. This will probably take a long time.'
-sclang $quarkinstall_path
+#$quarkinstall_path = $env:ChocolateyPackageFolder + '\tools\quarkinstall.sc'
+$quarkinstall_path = $env:programdata + '\chocolatey\tools\quarkinstall.sc'
+Write-Host 'Installing SuperDirt sound synth and sample library. This may take time.'
+cd $scpath
+.\sclang $quarkinstall_path
 
 ### install Tidal
-Write-Host "Installing tidal library. This will also take a long time." 
+Write-Host "Installing tidal library. This may take time." 
 cabal update
 cabal v1-install tidal
 
@@ -37,9 +35,12 @@ Write-Host "Installing Tidalcycles package to Pulsar"
 .\apm.cmd install tidalcycles
 Write-Host "Pulsar Install completed"
 
-Write-Host 'Done with Install.'
-Write-Host 'd1 $ sound "bd sn"'
+Write-Host "Done with TidalCyles package Install."
+Write-Host "Review the Chocolatey log file for details on errors: C:\ProgramData\chocolatey\logs\chocolatey.log"
+Write-Host "If SuperDirt or other Quarks didn't install properly, you can easily install them in the SuperCollider IDE."
+Write-Host "See User Docs install page for details."
 
-# Write-Host 'Please note that this installer has not installed an editor. For a complete Tidal system, we recommend you install pulsar.'
 Write-Host "If there were problems with the Pulsar install, you may need to manually install."
 Write-Host "Please see: http://tidalcycles.org/docs/getting-started/editor/Pulsar"
+Write-Host "Get started tidaling:"
+Write-Host 'd1 $ sound "bd [hh hh] sn cr"'
