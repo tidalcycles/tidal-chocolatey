9af126e5dfb65e5012f3806faafd9e697b77a39f
HighHarmonics <highharmonics@gmail.com>
2023-01-24 15:14:33 -0800

    update PowerShell-Pulsar install
    - change download URL to use github releases page with 1.101.0-beta release (download.pulsar-edit.dev is not reliable yet)
    - add options (-Wait, -PassThru) and -ArgumentList for Pulsar install for silent install with Start-Process cmdlet

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -21,25 +21,34 @@ cabal v1-install tidal
 
 ### install pulsar via download
 # NOTE: can be removed once pulsar is available in chocolatey
+##  $pulsarUrl from download.pulsar.dev not reliable, temporary use of pulsar github to 1.101.0-beta release
+##    will need to change with new releases
+##  Pulsar install done with Start-Process using -ArgumentList for silent install with no external window
+##    -Wait option insures that script will wait for install process to complete
+
 $wc = New-Object System.Net.WebClient
-$pulsarUrl = "https://download.pulsar-edit.dev/?os=windows&type=windows_setup"
-$downloadPath = Join-Path $env:HOMEPATH "Downloads\pulsarInstall.exe"
+$pulsarUrl = "https://github.com/pulsar-edit/pulsar/releases/download/v1.101.0-beta/Windows.Pulsar.Setup.1.101.0-beta.exe"
+#$pulsarUrl = "https://download.pulsar-edit.dev/?os=windows&type=windows_setup"
+
+$downloadPath = Join-Path $env:HOMEPATH "Downloads\pulsar-1.101.0-Install.exe"
 Write-Host "Downloading Pulsar to $downloadPath"
 $wc.DownloadFile($pulsarUrl, $downloadPath)
 
 Write-Host "Installing Pulsar - this may take awhile"
-Start-Process -FilePath $downloadPath
+Start-Process -FilePath $downloadPath -Wait -ArgumentList '/S','/v','qn' -PassThru
+
+## now install the TidalCycles package using apm
 cd $apmpath
 Write-Host "Installing Tidalcycles package to Pulsar"
 .\apm.cmd install tidalcycles
 Write-Host "Pulsar Install completed"
 
-Write-Host "Done with TidalCyles package Install."
+Write-Host "Done with TidalCyles Install."
 Write-Host "Review the Chocolatey log file for details on errors: C:\ProgramData\chocolatey\logs\chocolatey.log"
-Write-Host "If SuperDirt or other Quarks didn't install properly, you can easily install them in the SuperCollider IDE."
+Write-Host "If SuperDirt or other Quarks didn't install properly, you can install them in the SuperCollider IDE."
 Write-Host "See User Docs install page for details."
 
 Write-Host "If there were problems with the Pulsar or Pulsar plugin install, you may need to manually install."
-Write-Host "Please see: http://tidalcycles.org/docs/getting-started/editor/Pulsar"
+Write-Host "See: http://tidalcycles.org/docs/getting-started/editor/Pulsar"
 Write-Host "Get started tidaling:"
-Write-Host 'd1 $ sound "bd [hh hh] sn cr"'
+Write-Host 'd1 $ sound "bd [hh hh] sn cr" '
