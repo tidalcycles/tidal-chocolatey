68028eb0956075b67c872dc4d2da4afbb59f6fa9
HighHarmonics <highharmonics@gmail.com>
2023-04-11 16:22:43 -0700

    removed references to Pulsar

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -1,9 +1,5 @@
 ﻿$packageName= 'tidalcycles'
 
-# write new paths for SuperCollider, Pulsar and Pulsar apm 
-#   then load machine and user path values 
-
-
 $scPath = Get-AppInstallLocation SuperCollider
 $pulsarPath = Get-AppInstallLocation Pulsar
 $apmPath = Join-Path $pulsarPath "Resources\app\ppm\bin"
@@ -24,14 +20,14 @@ cabal v1-install tidal
 
 ## Install the TidalCycles package using apm
 cd $apmpath
-Write-Host "Installing TidalCycles package to Pulsar"
-Write-Host "Pulsar path: " $pulsarPath
+Write-Host "Installing TidalCycles package"
+Write-Host "path: " $pulsarPath
 .\apm.cmd install tidalcycles
 
 Write-Host "If SuperDirt or other Quarks didn't install properly, you can install them in the SuperCollider IDE."
 Write-Host "See User Docs install page for details.`n"
 
-Write-Host "If there were problems with the Pulsar or Pulsar plugin install, you may need to manually install."
+Write-Host "If there were problems with the plugin install, you may need to manually install."
 Write-Host "See: http://tidalcycles.org/docs/getting-started/editor/Pulsar `n"
-Write-Host "Get started tidaling:"
+Write-Host "Get started Tidaling:"
 Write-Host 'd1 $ sound "bd [hh hh] sn cr" '
