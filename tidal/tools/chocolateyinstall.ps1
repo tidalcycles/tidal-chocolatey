382fcf6be15588b1994e30d68b83da5158448cf2
il-mix <il-mix@yahoo.it>
2023-04-12 12:52:28 +0200

    Added superdirt as dependency; removed install steps from PS script. Removed typo from install script

--- tidal/tools/chocolateyinstall.ps1
+++ tidal/tools/chocolateyinstall.ps1
@@ -1,17 +1,8 @@
 ﻿$packageName= 'tidalcycles'
 
-$scPath = Get-AppInstallLocation SuperCollider
 $pulsarPath = Get-AppInstallLocation Pulsar
 $apmPath = Join-Path $pulsarPath "Resources\app\ppm\bin"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + $scPath + ";" + $apmPath + ";" + $pulsarPath
-
-### install SuperDirt
-Write-Host "Supercollider path: " $scPath
-$ChocolateyPackagePath = Get-ChocolateyPath -PathType 'PackagePath'
-$quarkinstall_path = $ChocolateyPackagePath + '\tools\quarkinstall.sc'
-Write-Host 'Installing SuperDirt sound synth and sample library. This may take time.'
-cd $scpath
-.\sclang $quarkinstall_path
 
 ### install Tidal
 Write-Host "Installing tidal library. This may take time." 
@@ -25,9 +16,9 @@ Write-Host "path: " $pulsarPath
 .\apm.cmd install tidalcycles
 
 Write-Host "If SuperDirt or other Quarks didn't install properly, you can install them in the SuperCollider IDE."
-Write-Host "See User Docs install page for details.`n"
+Write-Host "See User Docs install page for details."
 
 Write-Host "If there were problems with the plugin install, you may need to manually install."
-Write-Host "See: http://tidalcycles.org/docs/getting-started/editor/Pulsar `n"
+Write-Host "See: http://tidalcycles.org/docs/getting-started/editor/Pulsar"
 Write-Host "Get started Tidaling:"
 Write-Host 'd1 $ sound "bd [hh hh] sn cr" '
